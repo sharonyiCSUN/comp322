9b25b1ac1644b221a0f1e2cdae8dfcbbc0ab2369
sharonyiCSUN <60827742+sharonyiCSUN@users.noreply.github.com>
2020-02-16 22:29:02 -0800

    Update SYLab0.c

--- SYLab0.c
+++ SYLab0.c
@@ -6,76 +6,122 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-	char *charVal[] = {"     NUL", "     SOH", "     STX", "     ETX", "     EOT", "     ENQ", "     ACK",
-			   "     BEL", "      BS", "      HT", "      LF", "      VT", "      FF", "      CR",
-			   "      SO", "      SI", "     DLE", "     DC1", "     DC2", "     DC3", "     DC4",
-			   "     NAK", "     SYN", "     ETB", "     CAN", "      EM", "     SUB", "     ESC",
-			   "      FS", "      GS","      RS", "      US", "   SPACE"};
-	char lastVal[] = "     DEL";
-
-char readchar;
-    int readfromfile;
-    char filearray[9];
-    char ASCIIarray[8];
-	int sumoriginal = 0;
-    int arraycounter = 0;
-	readfromfile = open(argv[1], O_RDONLY);
-
-printf("Original ASCII    Decimal  Parity   \n");
-    printf("-------- -------- -------- -------- \n");
-
-
-
-    while((read(readfromfile, &readchar, 1)) != 0){
-		if(readfromfile < 0){
-			printf("Error handling file\n");
-			exit(0);
-		}
-		if((readchar != ' ') || (readchar != '\n')){
-			filearray[arraycounter] = readchar;
-			arraycounter++;
-		}
-
-		if((readchar == ' ')) { //|| (readchar != '\n')){
-			int sumoriginal = 0;
-			unsigned leng = (unsigned) strlen(filearray);
-			if(leng < 9){
-				for(int z = leng; z < 9; z++){
-					if((filearray[z] == ' '))// || (filearray[z] != '\n'))
-					filearray[z] = '0';
-				}
-				filearray[8] = '\0';
-			}
-			
-			for(int k = 0; k < 8; k++){
-				ASCIIarray[k] = filearray[k+1];
-				sumoriginal += (int)filearray[k];
-			}
+void printHeader(){
+  printf("Original ASCII    Decimal  Parity   \n");
+  printf("-------- -------- -------- -------- \n");
 }
 
-	char *ASCIIpt;
-            char ASCIIc = strtol(ASCIIarray, &ASCIIpt, 2);
-            int dec = (int)ASCIIc;
+char *charVal[] = {"     NUL", "     SOH", "     STX", "     ETX", "     EOT", "     ENQ", "     ACK",  //Dictionary for 0-32 ASCII characters
+		   "     BEL", "      BS", "      HT", "      LF", "      VT", "      FF", "      CR",
+		   "      SO", "      SI", "     DLE", "     DC1", "     DC2", "     DC3", "     DC4",
+		   "     NAK", "     SYN", "     ETB", "     CAN", "      EM", "     SUB", "     ESC", 
+		   "      FS", "      GS","      RS", "      US", "   SPACE"};
+char lastVal[] = "     DEL"; //Dictionary for 127 ASCII character
 
-	if(dec < 10)
-		printf("%s %s        %i ", filearray, charVal[dec], dec);
-            if((dec > 9) && (dec < 33))
-		printf("%s %s       %i ", filearray, charVal[dec], dec);
-            if((dec > 32) && (dec < 100))
-		printf("%s        %c       %i ", filearray, ASCIIc, dec);
-            if((dec > 101) && (dec <127) )
-		printf("%s        %c      %i ", filearray, ASCIIc, dec);
-            if(dec == 127){printf("%s %s      %i ", filearray, lastVal, dec);
-            if(sumoriginal%2 == 0)
-		printf("EVEN\n");
-			else
-		printf("ODD\n");
+void calcStuff(char input[]){ //if the flags are good, we can now start to print out the converted output
+  //int result = strcmp(input, "-");
+    
+  char ASCIIstr[8];
 
-			   arraycounter = 0;
-		}
-//	sumoriginal = 0;
-	}
-	return 0;
+  char* copiedstr = input;
+  unsigned length = (unsigned)strlen(input);
+  if(length < 9){
+    for(int j = length; j <= 8; j++)
+      copiedstr[j] = '0';
+    copiedstr[8] = '\0';
+  }
+  
+  int sumoforiginal = 0;
+  for(int k = 0; k < 8; k++){
+    ASCIIstr[k] = copiedstr[k+1];
+    sumoforiginal += (int)copiedstr[k];
+  }
+
+  char *ASCIIptr;
+  char ASCIIchar = strtol(ASCIIstr, &ASCIIptr, 2);
+  int decimal = (int)ASCIIchar;
+
+  
+  if(decimal < 10)
+    printf("%s %s        %i ", copiedstr, charVal[decimal], decimal);
+  if((decimal > 9) && (decimal < 33))
+    printf("%s %s       %i ", copiedstr, charVal[decimal], decimal);
+  if((decimal > 32) && (decimal < 100))
+    printf("%s        %c       %i ", copiedstr, ASCIIchar, decimal);
+  if((decimal > 101) && (decimal <127) ) 
+    printf("%s        %c      %i ", copiedstr, ASCIIchar, decimal);
+  if(decimal == 127)
+    printf("%s %s      %i ", copiedstr, lastVal, decimal);
+
+  if(sumoforiginal%2 == 0)
+    printf("EVEN\n");
+  else
+    printf("ODD\n");
+}
+
+
+void padRight(char *byteRaw){
+//  int byteRawSize = (int) strlen(byteRaw);
+  char arrSize[8]; //8 bits + 1 terminator
+  int endFlag = 0;
+  for(int i = 0; i < 8; i++){ //insert padding
+    if((byteRaw[i] == '0' || byteRaw[i] == '1') && endFlag == 0){
+      arrSize[i] = byteRaw[i];
+    }
+    else{
+      endFlag = 1;
+      arrSize[i] = '0';
+    }
+  }
+  calcStuff(arrSize);
+  
+}
+
+
+int main(int argc, char *argv[]) {
+  //read the command input
+  if(argc > 1){
+  
+    int file = 1; //assume there is a file
+    int readfromfile = open(argv[1], O_RDONLY);
+    if(readfromfile < 0){
+      file = 0; //set the flag;
+    }
+    
+    //it's cli input
+    if(file == 0){
+      printHeader();
+      for (int i = 1; i < argc; ++i){
+        padRight(argv[i]);
+      }
+    }
+    
+    //it's a file
+    if(file == 1){
+      printHeader();
+      char currentByte[8];
+      char readchar;
+      int index = 0;
+      while(read(readfromfile, &readchar, 1)){
+        index++;
+        if(readchar == '0' || readchar == '1'){
+          currentByte[index-1] = readchar;
+        }
+        if(readchar == ' '){ //found a space, then we calculate
+          padRight(currentByte);
+          index = 0;
+          for(int i = 0; i < 8; i++){ //null the array
+            currentByte[i] = 0;
+          }
+        }
+      }
+   	if(currentByte[0] == '0' || currentByte[0] == '1') //this checks if the array still has content in it, if it does, it goes through the iteration one last time to print out the last output
+		padRight(currentByte);
+    }
+  }
+  else{
+    exit(0);
+  }
+  return 0;
 }
 
