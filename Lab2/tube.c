932c408a3f40e64ca4e57ae8198af822befb7a17
Sharon Yi <sharon.yi.76@my.csun.edu>
2020-04-07 01:02:13 -0700

    Lab 2 Tube Update Updated source code of Tube

--- Lab2/tube.c
+++ Lab2/tube.c
@@ -1,9 +1,53 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[], char *envp[]){
 
-}
+  int pipePara[2];
+  pid_t cpid;
 
+  if (pipe(pipePara) == -1){
+    fprint(stderr, "Pipe Failed.\n");
+    return 1;
+  }
+
+  cpid = fork();
+
+  //
+  if (cpid < 0){
+    printf("Fork Failed.\n");
+    return 1;
+  }
+
+  //child process
+  else if (cpid > 0){
+    close(pipePara[1]);
+
+    fprintf(stderr, "%s: $$ = %i\n", argv[1], cpid);
+    waitpid(cpid, status, 0);
+    fprintf(stderr, "%s: $? = %i\n", argv[1], status);
+
+    close(pipePara[0]);
+
+    close(pipePara[1]);
+
+  }
+  //parent process
+  else (cpid == 0){
+
+    char *vArg[argc-1];
+    char *pEnv[] = {NULL};
+
+    for(int i = 0; i < (argc - 1); i++){
+      vArg[i] = argv[i+1];
+    }
+    vArg[argc - 1] = NULL;
+    execve(vArg[0], vArg, pEnv);
+
+    exit(EXIT_SUCCESS);
+  }
+  return 0;
+}
