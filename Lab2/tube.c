5db8b54cf32b5dd308f91e7c1cf79b36c2f45f1c
Sharon Yi <sharon.yi.76@my.csun.edu>
2020-04-21 19:44:14 -0700

    FINALLY FINISHED TUBE Fixed the stupid error. stupid comma...

--- Lab2/tube.c
+++ Lab2/tube.c
@@ -3,49 +3,157 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[], char *envp[]){
+  int status1, status2, counter = 0;
+  int pipefd[2];
+  pid_t cpid1, cpid2;
+  char buf;
 
-  int pipePara[2];
-  pid_t cpid;
+  // int fileD = open("filename", O_WRONLY | O_APPEND);
+  // if(fileD < 0)
+  //   printf("Error opening the file\n");
 
-  if (pipe(pipePara) == -1){
-    fprint(stderr, "Pipe Failed.\n");
-    return 1;
+  if(argc < 2)
+    exit(EXIT_FAILURE);
+
+  if (pipe(pipefd) == -1){
+    perror("Pipe");
+    exit(EXIT_FAILURE);
   }
 
-  cpid = fork();
+   while(strcmp(argv[counter], ",") != 0){
+      counter++;
+    }
+  //counter = 1;
+  //printf("this: %i \n", counter);
+  char *nArgv1[counter];
+  char *nArgv2[argc - counter - 1];
 
-  //
-  if (cpid < 0){
+  int increment = 1;
+  for(int i = 0; i < counter - 1; i++){
+    nArgv1[i] = argv[increment++];
+  }
+  nArgv1[counter - 1] = NULL;
+
+  for(int i = 0; i < (argc - counter -1); i++){
+    nArgv2[i] = argv[++increment];
+  }
+  nArgv2[argc - counter - 1] = NULL;
+
+  cpid1 = fork();
+
+  if (cpid1 < 0){
     printf("Fork Failed.\n");
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   //child process
-  else if (cpid > 0){
-    close(pipePara[1]);
+  else if (cpid1 > 0){
+    //close(pipefd[1]);
 
-    fprintf(stderr, "%s: $$ = %i\n", argv[1], cpid);
-    waitpid(cpid, status, 0);
-    fprintf(stderr, "%s: $? = %i\n", argv[1], status);
+    cpid2 = fork();
 
-    close(pipePara[0]);
+    if (cpid2 < 0){
+      printf("Fork Failed.\n");
+      exit(EXIT_FAILURE);
+    }
+    else if (cpid2 == 0){
 
-    close(pipePara[1]);
+      dup2(pipefd[0], STDIN_FILENO);
 
+      if(close(pipefd[1] == -1)){
+        fprintf(stderr, "Cannot read pipe.\n");
+        exit(EXIT_FAILURE);
+      }
+
+      //char *vArg[argc-1];
+      char *pEnv[] = {NULL};
+
+      // for(int i = 0; i < (argc - 1); i++){
+      //   vArg[i] = argv[i+1];
+      // }
+
+      // while(read(pipefd[0], &buf, 1) > 0)
+      //   write(STDOUT_FILENO, &buf, 1);
+
+    //  write(STDOUT_FILENO, NULL, 1);
+
+      //close(pipefd[0]);
+      read(pipefd[0], nArgv2, (sizeof(nArgv2) + 1));
+      execve(nArgv2[0], nArgv2, pEnv);
+
+      //dup2(pipefd[1], 1);
+      // while(read(pipefd[0], &buf, 1) > 0)
+      //   write(STDOUT_FILENO, &buf, 1);
+      //
+      // write(STDOUT_FILENO, NULL, 1);
+      // close(pipefd[0]);
+    //  read(filefd[0],); //still working here!
+      //_exit(EXIT_SUCCESS);
+
+      // vArg[argc - 1] = NULL;
+      // execve(vArg[0], vArg, pEnv);
+      // execve(vArg[0], vArg, pEnv);
+
+      //exit(EXIT_SUCCESS);
+    }
+    else{
+
+    fprintf(stderr, "%s: $$ = %i\n", nArgv1[0], cpid1);
+    fprintf(stderr, "%s: $$ = %i\n", nArgv2[0], cpid2);
+
+    close(pipefd[0]);
+
+    write(pipefd[1], nArgv1, sizeof(nArgv1));
+    write(pipefd[1], nArgv2, sizeof(nArgv2));
+
+    close(pipefd[1]);
+
+    waitpid(cpid1, &status1, 0);
+    waitpid(cpid2, &status2, 0);
+    fprintf(stderr, "%s: $? = %i\n", nArgv1[0], status1);
+    fprintf(stderr, "%s: $? = %i\n", nArgv2[0], status2);
+
+
+    //write(p[1], argv[1],strlen(argv[1]));
+    //write(pipefd[1], nArgv2[1],strlen(argv[1]));
+    //close(pipefd[1]);
+    wait(NULL);
+    exit(EXIT_SUCCESS);
+    }
   }
   //parent process
-  else (cpid == 0){
+  else if (cpid1 == 0){
 
     char *vArg[argc-1];
-    char *pEnv[] = {NULL};
+    char *ppEnv[] = {NULL};
 
-    for(int i = 0; i < (argc - 1); i++){
-      vArg[i] = argv[i+1];
+    // for(int i = 0; i < (argc - 1); i++){
+    //   nArgv1[i] = argv[i+1];
+    // }
+    // for(int i = 0; i < (argc - counter - 1); i++){
+    //   nArgv2[i] = argv[i+1];
+    // }
+    // nArgv1[argc - 1] = NULL;
+    // execve(nArgv1[0], nArgv1, pEnv);
+    // nArgv2[argc - 1] = NULL;
+    // execve(nArgv2[0], nArgv2, pEnv);
+
+    if(close(pipefd[1] == -1)){
+      fprintf(stderr, "Cannot read pipe.\n");
+      exit(EXIT_FAILURE);
     }
-    vArg[argc - 1] = NULL;
-    execve(vArg[0], vArg, pEnv);
+
+    dup2(pipefd[0], STDIN_FILENO);
+    read(pipefd[0], nArgv1, (sizeof(nArgv1) + 1));
+    close(pipefd[1]);
+
+    execve(nArgv1[0], nArgv1, ppEnv);
+  //  _exit(EXIT_SUCCESS);
+
 
     exit(EXIT_SUCCESS);
   }
