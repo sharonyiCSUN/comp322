656bb55754f07107d8af773462b509822196c904
Sharon Yi <sharon.yi.76@my.csun.edu>
2020-05-08 22:33:20 -0700

    Lab4 updated lab4 source

---
+++ Lab4/dining-p.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+int eat(){
+int usleep();
+int rand();
+  return 0;
+}
+
+int think(){
+
+int usleep();
+return 0;
+}
+
+int main(int argc, char &argv[]){
+
+semaphore chopstick[5];
+
+do{
+  wait(chopstick[i]);
+  wait(chopstick[(i + 1) % i]);
+
+  signal(chopstick[i]);
+  signal(chopstick[(i + 1) % i]);
+}while(true);
+
+
+
+return 0;
+}
+
