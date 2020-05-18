4f34575b5916112116541b9ababbd272f7577c78
Sharon Yi <sharon.yi.76@my.csun.edu>
2020-05-17 20:08:59 -0700

    Fixed Lab 6 mole Argv was not on the correct index

--- Lab6/mole.c
+++ Lab6/mole.c
@@ -11,12 +11,12 @@ int main(int argc, char *argv[]){
 
   (void)argc;
   FILE *logfile;
-
+  char *filename = ""/lab6.log";
   char cwd[PATH_MAX];
 
   chdir("~/");
   strcat(cwd, getenv("HOME"));
-  strcat(cwd, "/lab6.log");
+  strcat(cwd, filename);
   printf("Path to logfile: &\n", cwd);
 
   logfile = fopen(cwd, "a");
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]){
   if(logfile == NULL)
     perror("File Error\n");
 
-  fprintf(logfile, "Pop %s\n", argv[0])
+  fprintf(logfile, "Pop %s\n", argv[1])
   fclose(logfile);
 
   return(0);
